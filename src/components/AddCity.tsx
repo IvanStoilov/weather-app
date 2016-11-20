201c89a815dc8b2df9fde322b1b7282d71b0b42d
Ivan Stoilov <stoilov.ivan@gmail.com>
2016-11-20 17:42:26 +0100

    Improve add city form

--- src/components/AddCity.tsx
+++ src/components/AddCity.tsx
@@ -3,43 +3,59 @@ import {addCity} from "../actions/city-list.actions";
 import {CityStoreComponent} from "../custom-typings/city-store-component";
 
 export class AddCity extends CityStoreComponent<{}, {cityName: string}> {
-  constructor() {
-    super();
-  }
-
-  componentWillMount() {
-    this.setState({cityName: ''});
-  }
-
-  render(): JSX.Element {
-    return ( 
-      <form onSubmit={this.handleAdd.bind(this)}>
-         <div className="item-input">
-          <input 
-            type="text"
-            value={this.state.cityName} 
-            onChange={this.handleChange.bind(this)} />
-
-          <button className="button button-fill" type="submit">Add city</button>
-        </div>
-      </form>
-    );
-  }
-
-  private handleAdd(event: Event) {
-    if (this.state.cityName === '') {
-      return ;
+    constructor() {
+        super();
     }
 
-    this.context.store.dispatch(addCity(this.state.cityName));
-    this.setState({cityName: ''});
-    
-    event.preventDefault(); // prevent the form from submitting
-  }
+    componentWillMount() {
+        this.setState({cityName: ''});
+    }
 
-  private handleChange(event : KeyboardEvent) {
-    this.setState({cityName: (event.target as HTMLInputElement).value});
-  }
+    render(): JSX.Element {
+        return (
+            <div>
+                <div className="content-block-title">
+                    <label for="cityName" >Enter a city:</label>
+                </div>
+                <form className="list-block" onSubmit={this.handleAdd.bind(this)}>
+                    <ul>
+                        <li>
+                            <div className="item-content">
+                                <div className="item-inner">
+                                    <div className="item-input">
+                                        <input
+                                            type="text"
+                                            id="cityName"
+                                            value={this.state.cityName}
+                                            onChange={this.handleChange.bind(this)} />
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+                    <p className="content-block">
+                        <a className="button button-fill" type="submit">Add city</a>
+                    </p>
+                </form>
+            </div>
+        );
+    }
+
+    private handleAdd(event: Event) {
+        if (this.state.cityName === '') {
+            return ;
+        }
+
+        this.context.store.dispatch(addCity(this.state.cityName));
+        this.setState({cityName: ''});
+
+        event.preventDefault(); // prevent the form from submitting
+    }
+
+    private handleChange(event : KeyboardEvent) {
+        this.setState({cityName: (event.target as HTMLInputElement).value});
+    }
 }
 
 AddCity.contextTypes = {
