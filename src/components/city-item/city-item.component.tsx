4626cb2808f410ebccd5b3d2960fe1fd176c05ae
Ivan Stoilov <stoilov.ivan@gmail.com>
2016-12-05 12:23:20 +0200

    Spinenr added and current temp design improvement

--- src/components/city-item/city-item.component.tsx
+++ src/components/city-item/city-item.component.tsx
@@ -1,112 +1,106 @@
 import * as React from "react";
 import {ICity} from "../../data/city";
 import {Forecast, Forecastday} from "../../custom-typings/forecast";
+import {Spinner} from "../spinner/spinner.component";
 import * as moment from "moment";
 
 interface ICityItemProps {
-  city: ICity;
-  onDelete: Function;
-  onReload: Function;
+    city: ICity;
+    onDelete: Function;
+    onReload: Function;
 }
 
 export class CityItem extends React.Component<ICityItemProps, {}> {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  render() : JSX.Element {
-    return (
-      <div className="card">
-        <div className="card-header">
-            <div className="col-33">
-              {this.getName()} <br/>
+    render() : JSX.Element {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    <div>
+                        {this.getName()} <br/>
+                    </div>
+                    <div>
+                        {this.props.city.isFetching ? <Spinner /> : null}
+                    </div>
+                    <div>
+                        {this.getCurrentTemperature()}
+                    </div>
+                    <div className="condition">
+                        {this.getCurrentConditionIcon()}
+                    </div>
+                </div>
+                <div className="card-content">
+                    <div className="card-content-inner">
+                        { /* this.getImageIfSet() */ }
+                        {this.getWeather()}
+                    </div>
+                </div>
+                <div className="card-footer">
+                    <button type="button" className="button" onClick={() => this.props.onDelete()}>Remove</button>
+                    <button type="button" className="button" onClick={() => this.props.onReload()}>Reload</button>
+                </div>
             </div>
-            <div className="col-33">
-              {this.getIfNecessarySpinner()}
-            </div>
-            <div className="col-33">
-              {this.getCurrentConditionIcon()}
-            </div>
-        </div>
-        <div className="card-content">
-          <div className="card-content-inner">
-            { /* this.getImageIfSet() */ }
-            {this.getWeather()}
-          </div>
-        </div>
-        <div className="card-footer">
-          <button type="button" className="button" onClick={() => this.props.onDelete()}>Remove</button>
-          <button type="button" className="button" onClick={() => this.props.onReload()}>Reload</button>
-        </div>
-      </div>
-    )
-  }
-
-  private getImageIfSet() : JSX.Element {
-    if (this.props.city.imageUrl) {
-      return <img className="small-image" src={this.props.city.imageUrl} />
+        )
     }
 
-    return null;
-  }
-
-  private getIfNecessarySpinner() : JSX.Element {
-    if (this.props.city.isFetching) {
-      return <span> Loading...</span>
-    }
-
-    return null;
-  }
-
-  private getWeather() : JSX.Element {
-    if (this.getForecast()) {
-      return (
-        <div className="row">
-          {this.getForecastDays(this.getForecast().forecast.forecastday)}
-          { /* <p>
-            {this.getForecast().current.condition.text}, {this.getForecast().current.temp_c}°</p>
-          <p>Last updated: {moment(this.props.city.weather.updatedAt).fromNow()}</p>
-          */ }
-        </div>
-      );
-    }
-
-    return null;
-  }
-
-  private getForecastDays(forecastDays: Forecastday[]) : JSX.Element[] {
-    return forecastDays.map(day => this.getForecastDay(day));
-  }
-
-  private getForecastDay(forecastDay: Forecastday) : JSX.Element {
-     return (
-         <div key={forecastDay.date} className="forecast-day col-20">
-           {moment(forecastDay.date).format('MMM D')}
-           <img src={forecastDay.day.condition.icon} className="condition-icon" alt=""/>
-           {Math.round(forecastDay.day.mintemp_c)}° / {Math.round(forecastDay.day.maxtemp_c)}°
-         </div>
-     );
-  }
-
-  private getName() : string {
-    if (this.getForecast()) {
-      return `${this.getForecast().location.name}, ${this.getForecast().location.country}`;
-    }
-
-    return this.props.city.name;
-  }
-
-    private getCurrentConditionIcon() : JSX.Element {
-        if (this.getForecast()) {
-            return <span>
-                {Math.round(this.getForecast().current.temp_c)}° <img className="condition-icon" src={this.getForecast().current.condition.icon} />
-                </span>;
+    private getImageIfSet() : JSX.Element {
+        if (this.props.city.imageUrl) {
+            return <img className="small-image" src={this.props.city.imageUrl} />
         }
 
         return null;
     }
 
-  private getForecast() : Forecast {
-    return this.props.city.weather && this.props.city.weather.forecast;
-  }
+    private getWeather() : JSX.Element {
+        if (this.getForecast()) {
+            return (
+                <div className="row">
+                    {this.getForecastDays(this.getForecast().forecast.forecastday)}
+                    { /* <p>
+                     {this.getForecast().current.condition.text}, {this.getForecast().current.temp_c}°</p>
+                     <p>Last updated: {moment(this.props.city.weather.updatedAt).fromNow()}</p>
+                     */ }
+                </div>
+            );
+        }
+
+        return null;
+    }
+
+    private getForecastDays(forecastDays: Forecastday[]) : JSX.Element[] {
+        return forecastDays.map(day => this.getForecastDay(day));
+    }
+
+    private getForecastDay(forecastDay: Forecastday) : JSX.Element {
+        return (
+            <div key={forecastDay.date} className="forecast-day col-20">
+                {moment(forecastDay.date).format('MMM D')}
+                <img src={forecastDay.day.condition.icon} className="condition-icon" alt=""/>
+                {Math.round(forecastDay.day.mintemp_c)}° / {Math.round(forecastDay.day.maxtemp_c)}°
+            </div>
+        );
+    }
+
+    private getName() : string {
+        if (this.getForecast()) {
+            return `${this.getForecast().location.name}, ${this.getForecast().location.country}`;
+        }
+
+        return this.props.city.name;
+    }
+
+    private getCurrentConditionIcon() : JSX.Element {
+        return this.getForecast() ? <img src={this.getForecast().current.condition.icon} /> : null;
+    }
+
+    private getCurrentTemperature() : string {
+        return this.getForecast() ? `${Math.round(this.getForecast().current.temp_c)} °` : null;
+    }
+
+    private getForecast() : Forecast {
+        return this.props.city.weather && this.props.city.weather.forecast;
+    }
 }
---
+++ src/components/spinner/spinner.component.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class Spinner extends React.Component<{}, {}> {
+    render() : JSX.Element {
+        return (
+            <div className="spinner"></div>
+        );
+    }
+}
